236697a0ed0b5bc9f40b2bf980392d1a7b7e9f06
P <wolski@csag.uct.ac.za>
2020-02-26 15:46:57 +0200

    still trying to cleanup

--- js/upload.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-$('#inputCsv').on('change', function() {
-$("#preview").html('');
-$("#preview").html('<img src="loader.gif" alt="Uploading...."/>');
-$("#upload_form").ajaxForm({
-//target: '#preview'
-}).submit();
-});
-});
